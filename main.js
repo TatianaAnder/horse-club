b18a7327353d44eddd14bef0862ae9e628d137d7
Tatiana Anderson <happytasha9933@gmail.com>
2025-03-21 15:26:20 +0400

    add style with js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+
+const pricingTable = document.querySelector('.panel__pricing-table');
+pricingTable.addEventListener('mouseover', () => {
+    pricingTable.style.backgroundColor = '#f9e298';
+});
+pricingTable.addEventListener('mouseout', () => {
+    pricingTable.style.backgroundColor = '#dad7cd';
+});
+
+
 const imgs = document.querySelectorAll('.pricing-img');
 
 imgs.forEach(image => {
@@ -23,8 +33,12 @@ const prices = document.querySelectorAll('.pricing-fiture-item');
 prices.forEach(price => {
     price.addEventListener('mouseover', () => {
         price.style.border = '1px solid #3a5a40aa';
+        price.style.borderRadius = '10px';
     });
     price.addEventListener('mouseout', () => {
         price.style.border = 'none';
     });
-})
+    price.addEventListener('click', () => {
+        price.classList.toggle('background');
+    });
+});
